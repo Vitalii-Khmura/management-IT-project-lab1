0200ebfe43aa8e87f9aa57a3f4f01a28bd61e52c
Vitalii Khmura <vitalikkhmura13@gmail.comw>
2023-10-16 17:26:31 +0300

    Created custom page index.html

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+from my_app.views import index
 
+urlpatterns = [
+    path("", index, name="index"),
 ]
 
 app_name = "my_app"
